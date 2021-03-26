d3b69ea901720160d0485fa235df58a5ff1cd468
Alisson Claudino <alisson.claudino@ufrgs.br>
2021-03-26 12:48:58 -0300

    feat: remove actions header (#5)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hello",
-    version="1.1.1",
+    version="1.1.2",
     description="Hello",
     packages=setuptools.find_packages(),
 )
--- utils/versioning/bump_version.sh
+++ utils/versioning/bump_version.sh
@@ -8,7 +8,7 @@ fi
 NEW_TAG=$(cat setup.py | grep -o [0-9].[0-9].[0-9])
 git config --global user.name "Github Actions"
 git config --global user.email "github-actions@github.com"
-#git config --unset 'http.https://github.com/.extraHeader'
+git config --unset 'http.https://github.com/.extraHeader'
 git remote set-url origin "https://claudinoac:$BUMPER_TOKEN@github.com/claudinoac/bump-me"
 git add setup.py
 git commit -m "chore: update package version to v$NEW_TAG"
